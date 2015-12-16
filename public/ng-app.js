ed44986dcae347f925f7c161b4841dd6ae13b1e4
KevGary <kevgary.dev@gmail.com>
2015-12-16 03:28:23 -0700

    testing docker api capacity

--- public/ng-app.js
+++ public/ng-app.js
@@ -6,15 +6,18 @@ app.constant('DB_URL', 'http://159.203.102.106:3001');
 
 app.controller('GlobalController', function ($scope, DockerFactory, DBFactory) {
   $scope.message = 'douche';
-  DockerFactory.dockerPost('function foo(){return 5*5*5;}') 
-    .then(function success(response) {
-      $scope.resultData = response.data;
-    })
-  DBFactory.dbGet()
-    .then(function success(response) {
-      console.log(response.data)
-      $scope.usersData = response.data;
-    })
+  for(var i = 0; i < 7; i++) {
+    DockerFactory.dockerPost('function foo(){return 5*5*5;}') 
+      .then(function success(response) {
+        $scope.resultData = response.data;
+        console.log(response.data);
+      })
+  }
+  // DBFactory.dbGet()
+  //   .then(function success(response) {
+  //     console.log(response.data)
+  //     $scope.usersData[i] = response.data;
+  //   })
 })
 
 app.factory('DockerFactory', function Docker($q, $http, API_URL) {
